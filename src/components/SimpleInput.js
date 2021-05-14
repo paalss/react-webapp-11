4c7279520b68b70493b3379751bbe42699fb37dc
paalss <paalss94@gmail.com>
2021-05-14 13:45:13 +0200

    legger til enkel feedback tekst og passende CSS styling

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,14 +13,19 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false)
       return
     }
+    setEnteredNameIsValid(true)
+
     console.log(enteredName);
     console.log(nameInputRef.current.value);
   };
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -27,6 +33,7 @@ const SimpleInput = (props) => {
           id="name"
           onChange={nameInputChangeHandler}
         />
+        {!enteredNameIsValid && <p className="error-text">fyll ut navn</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
