290d4b3a12a00d63fb8aabbfbbf78f00c0fb8f15
paalss <paalss94@gmail.com>
2021-05-14 14:34:00 +0200

    onblur validation og fjerne refs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,30 +1,39 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+
+    if (event.target.value.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
+  };
+
+  const nameInputBlur = () => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    setEnteredNameTouched(true)
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
+    setEnteredNameTouched(true);
+    if (enteredName.trim() !== "") {
+      setEnteredNameIsValid(true);
     }
     setEnteredNameIsValid(true);
 
-    console.log(enteredName);
-    console.log(nameInputRef.current.value);
+    // console.log(enteredName);
+    // console.log(nameInputRef.current.value);
   };
 
-  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInValid
     ? "form-control invalid"
@@ -34,9 +43,9 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
+          onBlur={nameInputBlur}
           onChange={nameInputChangeHandler}
         />
         {nameInputIsInValid && <p className="error-text">fyll ut navn</p>}
