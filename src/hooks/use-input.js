0440d198d96f7be5cbab3285d4fe220d1b1b58a0
paalss <paalss94@gmail.com>
2021-05-17 12:14:19 +0200

    git add commit småretting. 'git commit' med -a flag stager visst ikke nye filer slik som git 'add .'. Denne commiten har med det

---
+++ src/hooks/use-input.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredInput, setEnteredInput] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const inputIsValid = validateValue(enteredInput);
+  const showError = !inputIsValid && isTouched;
+
+  const inputChangeHandler = (event) => {
+    setEnteredInput(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredInput("");
+    setIsTouched(false);
+  };
+  return {
+    input: enteredInput,
+    isValid: inputIsValid,
+    showError,
+    inputChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
