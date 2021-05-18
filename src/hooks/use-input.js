e43c51142ca4015e6e5bbf0511130215d354bd0c
paalss <paalss94@gmail.com>
2021-05-18 14:14:22 +0200

    bytte ut useStates med useReducers (liten bonus. egentlig ikke nødvendig i dette brukscenarioet, men bare greit å gjøre for å få øvd seg litt)

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,43 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type==='INPUT') {
+    return { value: action.value, isTouched: state.isTouched}
+  }
+  if (action.type==='BLUR') {
+    return {isTouched: true, value: state.value}
+  }
+  if (action.type==='RESET') {
+    return {isTouched: false, value: ''}   
+  }
+  return inputStateReducer;
+};
 
 const useInput = (validateValue) => {
-  const [enteredInput, setEnteredInput] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
 
-  const inputIsValid = validateValue(enteredInput);
-  const showError = !inputIsValid && isTouched;
+  const inputIsValid = validateValue(inputState.value);
+  const showError = !inputIsValid && inputState.isTouched;
 
   const inputChangeHandler = (event) => {
-    setEnteredInput(event.target.value);
+    dispatch({type: 'INPUT', value: event.target.value})
   };
-
+  
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({type: 'BLUR'})
   };
 
   const reset = () => {
-    setEnteredInput("");
-    setIsTouched(false);
+    dispatch({type:'RESET'})
   };
-  
+
   return {
-    input: enteredInput,
+    input: inputState.value,
     isValid: inputIsValid,
     showError,
     inputChangeHandler,
