7518c18a94bc6b4293af0befe284138a3940792d
paalss <paalss94@gmail.com>
2021-05-19 21:20:48 +0200

    auto formaterer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -6,34 +6,37 @@ const initialInputState = {
 };
 
 const inputStateReducer = (state, action) => {
-  if (action.type==='INPUT') {
-    return { value: action.value, isTouched: state.isTouched}
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
   }
-  if (action.type==='BLUR') {
-    return {isTouched: true, value: state.value}
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
   }
-  if (action.type==='RESET') {
-    return {isTouched: false, value: ''}   
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
   }
   return initialInputState;
 };
 
 const useInput = (validateValue) => {
-  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
   const inputIsValid = validateValue(inputState.value);
   const showError = !inputIsValid && inputState.isTouched;
 
   const inputChangeHandler = (event) => {
-    dispatch({type: 'INPUT', value: event.target.value})
+    dispatch({ type: "INPUT", value: event.target.value });
   };
-  
+
   const inputBlurHandler = (event) => {
-    dispatch({type: 'BLUR'})
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    dispatch({type:'RESET'})
+    dispatch({ type: "RESET" });
   };
 
   return {
